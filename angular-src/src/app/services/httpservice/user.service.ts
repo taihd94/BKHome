cb223efe5b186607a30cc22498a6889ab8c409ef
taihd94 <taihd94@gmail.com>
2017-03-03 21:35:12 +0700

    remove localhost

--- angular-src/src/app/services/httpservice/user.service.ts
+++ angular-src/src/app/services/httpservice/user.service.ts
@@ -13,14 +13,14 @@ export class UserService {
   registerUser(user){
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
-    return this.http.post('http://localhost:3000/users/register', user, {headers: headers})
+    return this.http.post('/users/register', user, {headers: headers})
       .map(res => res.json());
   }
 
   authenticateUser(user){
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
-    return this.http.post('http://localhost:3000/users/authenticate', user, {headers: headers})
+    return this.http.post('/users/authenticate', user, {headers: headers})
       .map(res => res.json());
   }
 
@@ -30,7 +30,7 @@ export class UserService {
     console.log(this.authToken);
     headers.append('Authorization', this.authToken);
     headers.append('Content-Type', 'application/json');
-    return this.http.get('http://localhost:3000/users/profile', {headers: headers})
+    return this.http.get('/users/profile', {headers: headers})
       .map(res => res.json());
   }
 
--- public/main.bundle.js
+++ public/main.bundle.js
@@ -168,13 +168,13 @@ var UserService = (function () {
     UserService.prototype.registerUser = function (user) {
         var headers = new __WEBPACK_IMPORTED_MODULE_1__angular_http__["Headers"]();
         headers.append('Content-Type', 'application/json');
-        return this.http.post('http://localhost:3000/users/register', user, { headers: headers })
+        return this.http.post('/users/register', user, { headers: headers })
             .map(function (res) { return res.json(); });
     };
     UserService.prototype.authenticateUser = function (user) {
         var headers = new __WEBPACK_IMPORTED_MODULE_1__angular_http__["Headers"]();
         headers.append('Content-Type', 'application/json');
-        return this.http.post('http://localhost:3000/users/authenticate', user, { headers: headers })
+        return this.http.post('/users/authenticate', user, { headers: headers })
             .map(function (res) { return res.json(); });
     };
     UserService.prototype.getProfile = function () {
@@ -183,7 +183,7 @@ var UserService = (function () {
         console.log(this.authToken);
         headers.append('Authorization', this.authToken);
         headers.append('Content-Type', 'application/json');
-        return this.http.get('http://localhost:3000/users/profile', { headers: headers })
+        return this.http.get('/users/profile', { headers: headers })
             .map(function (res) { return res.json(); });
     };
     UserService.prototype.storeUserData = function (token, user) {
